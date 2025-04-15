17707a3cb28e10ae3ccf2b3cf878e36f884ef69c
Omar Lengua <omarns21@gmail.com>
2025-04-15 21:06:33 +0200

    Implement: changes in structure of home page, fix errors and improved sequence, change route of particles component

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
 
 export const router = createBrowserRouter([
     {
@@ -10,7 +13,19 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element: <Home />
-            }
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "projects",
+                element: <Projects />,
+            },
+            {
+                path: "contact",
+                element: <Contact />,
+            },
         ]
     }
 ]);
